7f73694ee604a20d81f206d001d1d35ab08740fb
jspieler <75164246+jspieler@users.noreply.github.com>
2023-08-30 18:40:15 +0200

    Added initial set of colormaps and use_colormap function

---
+++ aesthema_colormaps/__init__.py
@@ -0,0 +1,2 @@
+from .colormaps import use_colormap, create_colormap, Colormaps
+from .colors import Colors
---
+++ aesthema_colormaps/colormaps.py
@@ -0,0 +1,57 @@
+"""Provides custom colormaps for Matplotlib."""
+from enum import Enum
+from typing import List, Optional
+
+import matplotlib
+from cycler import cycler
+
+from .colors import Colors
+
+
+def create_colormap(colors: List[Colors]) -> matplotlib.colors.ListedColormap:
+    return matplotlib.colors.ListedColormap(
+        [tuple(color_value / 255 for color_value in color.value) for color in colors]
+    )
+
+
+class Colormaps(Enum):
+    TBD = create_colormap(
+        [
+            Colors.TEA_GREEN,
+            Colors.WHEAT,
+            Colors.DESERT_SAND,
+            Colors.OLD_ROSE,
+            Colors.WENGE,
+            Colors.CINEREOUS,
+        ]
+    )
+    TBD2 = create_colormap(
+        [
+            Colors.GRAY,
+            Colors.HEATHER,
+            Colors.INTENSE_ROSE,
+            Colors.SALMON,
+            Colors.OLD_ORANGE,
+            Colors.LEMON,
+        ]
+    )
+    TBD3 = create_colormap(
+        [
+            Colors.LIGHT_BLACK,
+            Colors.LIGHT_PETROL,
+            Colors.LIGHT_TURQUOISE,
+            Colors.LIGHT_OCEAN,
+            Colors.IVORY,
+            Colors.LIGHT_ORANGE,
+            Colors.ORANGE,
+            Colors.DARK_ORANGE,
+            Colors.RED,
+            Colors.BERRY,
+        ]
+    )
+
+
+def use_colormap(cmap: Optional[Colormaps] = Colormaps.TBD) -> None:
+    matplotlib.pyplot.rcParams["axes.prop_cycle"] = cycler(
+        color=[cmap.value(i) for i in range(cmap.value.N)]
+    )
