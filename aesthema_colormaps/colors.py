fef729467810d135f39cd802d457f32d6b262bbe
jspieler <75164246+jspieler@users.noreply.github.com>
2023-08-30 18:39:35 +0200

    Added initial set of colors

---
+++ aesthema_colormaps/colors.py
@@ -0,0 +1,27 @@
+"""Provides color definitions."""
+from enum import Enum
+
+
+class Colors(Enum):
+    TEA_GREEN = (221, 232, 185)
+    WHEAT = (232, 210, 174)
+    DESERT_SAND = (215, 178, 157)
+    OLD_ROSE = (203, 133, 137)
+    WENGE = (121, 100, 101)
+    CINEREOUS = (133, 114, 115)
+    GRAY = (142, 142, 142)
+    HEATHER = (177, 155, 173)
+    INTENSE_ROSE = (228, 163, 178)
+    SALMON = (255, 177, 157)
+    OLD_ORANGE = (255, 207, 126)
+    LEMON = (249, 248, 113)
+    LIGHT_BLACK = (0, 18, 25)
+    LIGHT_PETROL = (0, 95, 115)
+    LIGHT_TURQUOISE = (10, 147, 150)
+    LIGHT_OCEAN = (148, 210, 189)
+    IVORY = (233, 216, 166)
+    LIGHT_ORANGE = (238, 155, 0)
+    ORANGE = (202, 103, 2)
+    DARK_ORANGE = (187, 62, 3)
+    RED = (174, 32, 18)
+    BERRY = (155, 34, 38)
