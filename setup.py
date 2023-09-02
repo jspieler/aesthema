9be4a2ef87159150e638daaa0d1c59171a88c1d7
jspieler <75164246+jspieler@users.noreply.github.com>
2023-09-02 18:50:15 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+setup(name='aesthema_colormaps',
+      version='0.0.1',
+      description='Modern & visually appealing colormaps for Matplotlib',
+      author='jspieler',
+      license='MIT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages = ['aesthema_colormaps'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+      ],
+      install_requires=['cycler', 'matplotlib'],
+      python_requires='>=3.8',
+      extras_require={
+        'linting': [
+            "pylint",
+            "mypy",
+        ],
+        'testing': [
+            "numpy"
+        ],
+      },
+      include_package_data=True)
